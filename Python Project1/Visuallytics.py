52b0da43b4f5ca455828a17bc0eacee7cd49c192
Sunil Shinde <sunilshinde96881@gmail.com>
2025-08-08 23:08:26 +0530

    Path change

--- Python Project1/Visuallytics.py
+++ Python Project1/Visuallytics.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-file_path = 'Python-Project/Temp_data.csv'
+file_path = 'Python-Project\Python Project1\Temp_data.csv'
 df = pd.read_csv(file_path)
 
 
